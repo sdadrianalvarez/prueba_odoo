4380808c1d94251cf5c33ec2533c7202bebebcf0
sdadrianalvarez <sdadrianalvarez@users.noreply.github.com>
2019-08-21 16:12:48 +0000

    Se descomentaron archivos con código de ejemplo

--- modulo_excel/models/models.py
+++ modulo_excel/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class modulo_excel(models.Model):
-#     _name = 'modulo_excel.modulo_excel'
+ class modulo_excel(models.Model):
+     _name = 'modulo_excel.modulo_excel'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
+
+     @api.depends('value')
+     def _value_pc(self):
+         self.value2 = float(self.value) / 100
