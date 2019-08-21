a4898c6e82a74aac0847df36d26c21969541edb4
sdadrianalvarez <sdadrianalvarez@users.noreply.github.com>
2019-08-21 17:12:26 +0000

    Error indentación en  controllers.py

--- modulo_excel/controllers/controllers.py
+++ modulo_excel/controllers/controllers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
- class ModuloExcel(http.Controller):
+class ModuloExcel(http.Controller):
      @http.route('/modulo_excel/modulo_excel/', auth='public')
      def index(self, **kw):
          return "Hello, world"
