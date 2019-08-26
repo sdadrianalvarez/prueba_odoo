8e2de03699fe97e2b93c4f822ad33b0b10e2787a
sdadrianalvarez <sdadrianalvarez@users.noreply.github.com>
2019-08-26 17:16:46 +0000

    orreccion de error en modelo

--- modulo_excel/controllers/controllers.py
+++ modulo_excel/controllers/controllers.py
@@ -2,18 +2,18 @@
 from odoo import http
 
 class ModuloExcel(http.Controller):
-     @http.route('/modulo_excel/modulo_excel/', auth='public')
+     @http.route('/modulo_excel/lista_archivos/', auth='public')
      def index(self, **kw):
          return "Hello, world"
 
-     @http.route('/modulo_excel/modulo_excel/objects/', auth='public')
+     @http.route('/modulo_excel/lista_archivos/objects/', auth='public')
      def list(self, **kw):
          return http.request.render('modulo_excel.listing', {
-             'root': '/modulo_excel/modulo_excel',
+             'root': '/modulo_excel/lista_archivos',
              'objects': http.request.env['modulo_excel.lista_archivos'].search([]),
          })
 
-     @http.route('/modulo_excel/modulo_excel/objects/<model("modulo_excel.lista_archivos"):obj>/', auth='public')
+     @http.route('/modulo_excel/lista_archivos/objects/<model("modulo_excel.lista_archivos"):obj>/', auth='public')
      def object(self, obj, **kw):
          return http.request.render('lista_archivos.object', {
              'object': obj
